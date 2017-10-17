75b00bf90fdc6989f50d31ab35c51a50489176c5
Juho Hämäläinen <juho.hamalainen@jolla.com>
2017-10-17 10:26:22 +0300

    [streamrestore] Allow handling both builtin and external events.

--- src/plugins/streamrestore/plugin.c
+++ src/plugins/streamrestore/plugin.c
@@ -36,6 +36,15 @@
 
 #define BASE_VOLUME(base, max, volume) (base + volume * (max - base) / 100)
 
+#define TYPE_DEFAULT    (0)
+#define TYPE_BUILTIN    (1)
+#define TYPE_EXTERNAL   (2)
+
+#define USE_VAL1        (0)
+#define USE_VAL_MIN     (1)
+
+#define VOLUME_MAX      (100)
+
 N_PLUGIN_NAME        ("stream-restore")
 N_PLUGIN_VERSION     ("0.2")
 N_PLUGIN_DESCRIPTION ("Volumes using Pulseaudio DBus stream restore")
@@ -48,7 +57,21 @@ typedef struct transform_entry {
     int max;
 } transform_entry;
 
-static GHashTable *stream_restore_role_map = NULL;
+typedef struct context_entry {
+    char *key;
+    guint type;
+    guint use_val;
+    int val;
+} context_entry;
+
+typedef struct role_entry {
+    guint ref;
+    char *role;
+    GSList *contexts;
+    int volume;
+} role_entry;
+
+static GHashTable *stream_restore_role_map = NULL; /* contains GSLists of role_entry structs */
 static GList      *transform_entries       = NULL; /* contains transform_entry entries */
 static guint       output_route_type_val   = 0;
 
@@ -74,6 +97,214 @@ output_route_type ()
     return TYPE_DEFAULT;
 }
 
+static role_entry*
+role_entry_ref (role_entry *e)
+{
+    e->ref++;
+    return e;
+}
+
+static void
+context_entry_free (gpointer data)
+{
+    context_entry *c = data;
+    g_free (c->key);
+    g_free (c);
+}
+
+static void
+role_entry_free (role_entry *e)
+{
+    g_slist_free_full (e->contexts, context_entry_free);
+    g_free (e->role);
+    g_free (e);
+}
+
+static void
+role_entry_unref (role_entry *e)
+{
+    g_assert (e->ref);
+    e->ref--;
+    if (e->ref == 0) {
+        N_DEBUG (LOG_CAT "deleting entry for role '%s'", e->role);
+        role_entry_free (e);
+    }
+}
+
+static context_entry*
+context_entry_new (const char *key, guint type, guint use_val)
+{
+    context_entry *c = g_new0 (context_entry, 1);
+    c->key = g_strdup (key);
+    c->type = type;
+    c->use_val = use_val;
+    return c;
+}
+
+static void
+role_entry_add_context (role_entry *e, const char *key, guint type, guint use_val)
+{
+    context_entry *c;
+
+    c = context_entry_new (key, type, use_val);
+    e->contexts = g_slist_append (e->contexts, c);
+    role_entry_ref (e);
+}
+
+static gboolean
+parse_rule (role_entry *e, guint type, char *str)
+{
+    char *s;
+
+    g_assert (e);
+
+    if (g_str_has_prefix (str, "min:") &&
+        (s = strstr (str + strlen ("min:"), ","))) {
+
+        s[0] = '\0';
+        s = s + 1;
+        str = str + strlen ("min:");
+        if (strlen (s) == 0 || strlen (str) == 0)
+            return FALSE;
+
+        role_entry_add_context (e, str, type, USE_VAL_MIN);
+        role_entry_add_context (e, s, type, USE_VAL_MIN);
+    } else
+        role_entry_add_context (e, str, type, USE_VAL1);
+
+    return TRUE;
+}
+
+static gboolean
+parse_rules (role_entry *e, const char *identifier, guint type, const char *str)
+{
+    gboolean ret = FALSE;
+    char *tmp;
+    char *s;
+    char *end;
+
+    tmp = g_strdup (str);
+
+    if ((s = g_strrstr (tmp, identifier)) &&
+        (end = strstr (s + strlen (identifier), ")"))) {
+
+        end[0] = '\0';
+        s = s + strlen (identifier);
+        if (strlen (s) < 1 || !parse_rule (e, type, s))
+            goto done;
+        ret = TRUE;
+    }
+
+done:
+    g_free (tmp);
+    return ret;
+}
+
+static void
+role_entry_parse_rules (role_entry *e, const char *str)
+{
+    g_assert (e);
+    g_assert (str);
+
+    parse_rules (e, "builtin@(", TYPE_BUILTIN, str);
+    parse_rules (e, "external@(", TYPE_EXTERNAL, str);
+
+    if (!e->contexts)
+        role_entry_add_context (e, str, TYPE_DEFAULT, USE_VAL1);
+}
+
+static role_entry*
+role_entry_new (const char *role, const char *str)
+{
+    role_entry *e;
+
+    e = g_new0 (role_entry, 1);
+    e->ref = 0; /* role entry's reference count == context list size */
+    e->role = g_strdup (role);
+    role_entry_parse_rules (e, str);
+
+    N_DEBUG (LOG_CAT "new role entry '%s'", e->role);
+
+    return e;
+}
+
+static gboolean
+role_entry_get_volume (role_entry *entry, int *volume)
+{
+    GSList *i;
+    context_entry *c;
+
+    g_assert (entry);
+    g_assert (volume);
+
+    *volume = VOLUME_MAX;
+
+    for (i = entry->contexts; i; i = i->next) {
+        c = i->data;
+
+        if (c->type == TYPE_DEFAULT) {
+            *volume = c->val;
+            break;
+        }
+
+        if (output_route_type() == c->type) {
+            if (c->use_val == USE_VAL1) {
+                *volume = c->val;
+                break;
+            } else
+                *volume = *volume < c->val ? *volume : c->val;
+        }
+    }
+
+    if (entry->volume != *volume) {
+        entry->volume = *volume;
+        return TRUE;
+    }
+
+    return FALSE;
+}
+
+static gboolean
+role_entry_update_volume (role_entry *entry, const char *key, int volume)
+{
+    gboolean volume_changed = FALSE;
+    GSList *i;
+    context_entry *c;
+
+    g_assert (entry);
+    g_assert (key);
+
+    for (i = entry->contexts; i; i = i->next) {
+        c = i->data;
+
+        if (c->type == TYPE_DEFAULT) {
+            if (c->val != volume) {
+                c->val = volume;
+                volume_changed = TRUE;
+            }
+            break;
+        }
+
+        if (!g_strcmp0 (key, c->key)) {
+            if (c->val != volume) {
+                c->val = volume;
+                volume_changed = TRUE;
+            }
+        }
+    }
+
+    return volume_changed;
+}
+
+static gboolean
+role_entry_update_and_get_volume (role_entry *entry, const char *key, int volume, int *new_volume)
+{
+    if (role_entry_update_volume (entry, key, volume))
+        return role_entry_get_volume (entry, new_volume);
+
+    return FALSE;
+}
+
 static void
 init_done_cb (NHook *hook, void *data, void *userdata)
 {
@@ -84,7 +315,9 @@ init_done_cb (NHook *hook, void *data, void *userdata)
     NCore          *core    = n_plugin_get_core (plugin);
     NContext       *context = n_core_get_context (core);
     const char     *key     = NULL;
-    const char     *role    = NULL;
+    GSList         *entries = NULL;
+    GSList         *i       = NULL;
+    role_entry     *entry   = NULL;
     const NValue   *value   = NULL;
     int             volume  = 0;
     GHashTableIter  iter;
@@ -98,22 +331,27 @@ init_done_cb (NHook *hook, void *data, void *userdata)
        about. */
 
     g_hash_table_iter_init (&iter, stream_restore_role_map);
-    while (g_hash_table_iter_next (&iter, (gpointer) &key, (gpointer) &role)) {
+    while (g_hash_table_iter_next (&iter, (gpointer) &key, (gpointer) &entries)) {
+
         value = (NValue*) n_context_get_value (context, key);
         if (!value) {
-            N_WARNING (LOG_CAT "no value for found role '%s', key '%s' from context",
-                role, key);
+            N_DEBUG (LOG_CAT "no value found for role '%s', key '%s' from context",
+                             entry->role, key);
             continue;
         }
 
         if (n_value_type (value) != N_VALUE_TYPE_INT) {
             N_WARNING (LOG_CAT "invalid value type for role '%s', key '%s'",
-                role, key);
+                               entry->role, key);
             continue;
         }
 
-        volume = n_value_get_int (value);
-        (void) volume_controller_update (role, volume);
+        for (i = entries; i; i = i->next) {
+            entry = i->data;
+            volume = n_value_get_int (value);
+            if (role_entry_update_and_get_volume (entry, key, volume, &volume))
+                volume_controller_update (entry->role, volume);
+        }
     }
 }
 
@@ -190,6 +428,23 @@ error:
 }
 
 static void
+hash_table_add_cb (gpointer data, gpointer user_data)
+{
+    context_entry  *c       = data;
+    role_entry     *e       = user_data;
+    GSList         *entries = NULL;
+
+    if ((entries = g_hash_table_lookup (stream_restore_role_map, c->key))) {
+        entries = g_slist_append (entries, e);
+    } else {
+        entries = g_slist_append (entries, e);
+        g_hash_table_insert (stream_restore_role_map,
+                             g_strdup (c->key),
+                             entries);
+    }
+}
+
+static void
 volume_add_role_key_cb (const char *key, const NValue *value, gpointer userdata)
 {
     (void) key;
@@ -197,14 +452,15 @@ volume_add_role_key_cb (const char *key, const NValue *value, gpointer userdata)
     (void) userdata;
 
     const char *new_key = NULL;
+    role_entry *new_entry;
     int         volume  = 0;
 
     if (g_str_has_prefix (key, ROLE_KEY_PREFIX)) {
         new_key = (const char*) key + strlen (ROLE_KEY_PREFIX);
 
         if (new_key) {
-            g_hash_table_replace (stream_restore_role_map,
-                n_value_dup_string ((NValue*) value), g_strdup (new_key));
+            if ((new_entry = role_entry_new (new_key, n_value_get_string (value))))
+                g_slist_foreach (new_entry->contexts, hash_table_add_cb, new_entry);
         }
     }
     else if (g_str_has_prefix (key, SET_KEY_PREFIX)) {
@@ -231,7 +487,9 @@ void context_value_changed_cb (NContext *context, const char *key,
     (void) old_value;
     (void) userdata;
 
-    const char *role   = NULL;
+    GSList     *entries;
+    GSList     *i;
+    role_entry *entry;
     int         volume = 0;
 
     if (!g_strcmp0 (key, CONTEXT_ROUTE_OUTPUT_TYPE_KEY)) {
@@ -240,17 +498,29 @@ void context_value_changed_cb (NContext *context, const char *key,
         return;
     }
 
-    role = g_hash_table_lookup (stream_restore_role_map, key);
-    if (!role)
+    entries = g_hash_table_lookup (stream_restore_role_map, key);
+    if (!entries)
         return;
 
     if (n_value_type ((NValue*) new_value) != N_VALUE_TYPE_INT) {
-        N_WARNING (LOG_CAT "invalid value type for role '%s', key '%s'",
-            role, key);
+        N_WARNING (LOG_CAT "invalid value type for role context key '%s'", key);
+        return;
     }
 
-    volume = n_value_get_int ((NValue*) new_value);
-    (void) volume_controller_update (role, volume);
+    for (i = entries; i; i = i->next) {
+        entry = i->data;
+        volume = n_value_get_int ((NValue*) new_value);
+        if (role_entry_update_and_get_volume (entry, key, volume, &volume))
+            volume_controller_update (entry->role, volume);
+    }
+}
+
+static void
+entry_list_free (gpointer data)
+{
+    GSList *entries = data;
+
+    g_slist_free_full (entries, (GDestroyNotify) role_entry_unref);
 }
 
 N_PLUGIN_LOAD (plugin)
@@ -260,7 +530,7 @@ N_PLUGIN_LOAD (plugin)
     NProplist *params  = NULL;
 
     stream_restore_role_map = g_hash_table_new_full (g_str_hash, g_str_equal,
-        g_free, g_free);
+                                                     g_free, entry_list_free);
 
     volume_controller_initialize ();
 
