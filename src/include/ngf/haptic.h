93b1e7c450acc182c42df5ce5527ddc4258bad62
Juho Hämäläinen <juho.hamalainen@jolla.com>
2022-11-17 12:01:22 +0200

    haptic: Fix indentation.

--- src/include/ngf/haptic.h
+++ src/include/ngf/haptic.h
@@ -44,14 +44,14 @@
  * The haptic event type is used to filter out playback in case user has
  * disabled the setting for certain type of haptic feedback.
  */
-#define N_HAPTIC_TYPE_KEY         "haptic.type"
+#define N_HAPTIC_TYPE_KEY               "haptic.type"
 
-#define N_HAPTIC_TYPE_TOUCH       "touch"
-#define N_HAPTIC_TYPE_EVENT       "event"
+#define N_HAPTIC_TYPE_TOUCH             "touch"
+#define N_HAPTIC_TYPE_EVENT             "event"
 
-#define N_HAPTIC_EFFECT_KEY       "haptic.effect"
+#define N_HAPTIC_EFFECT_KEY             "haptic.effect"
 
-#define N_HAPTIC_EFFECT_DEFAULT   "default"
+#define N_HAPTIC_EFFECT_DEFAULT         "default"
 
 /**
  * haptic.repeat is generic catch-all boolean that when defined and set
@@ -83,29 +83,29 @@
  *
  * alarm and ringtone effects should repeat indefinitely.
  */
-#define N_HAPTIC_EFFECT_DRAG_START     "drag_start"
-#define N_HAPTIC_EFFECT_RELEASE_WEAK   "release_weak"
-#define N_HAPTIC_EFFECT_DRAG_FAIL      "drag_fail"
-#define N_HAPTIC_EFFECT_DRAG_BOUNDARY  "drag_boundary"
-#define N_HAPTIC_EFFECT_TOUCH_WEAK     "touch_weak"
-#define N_HAPTIC_EFFECT_DRAG_END       "drag_end"
-#define N_HAPTIC_EFFECT_RELEASE        "release"
-#define N_HAPTIC_EFFECT_TOUCH          "touch"
-#define N_HAPTIC_EFFECT_RELEASE_STRONG "release_strong"
-#define N_HAPTIC_EFFECT_TOUCH_STRONG   "touch_strong"
-#define N_HAPTIC_EFFECT_SHORT          "short"
-#define N_HAPTIC_EFFECT_STRONG         "strong"
-#define N_HAPTIC_EFFECT_LONG           "long"
-#define N_HAPTIC_EFFECT_NOTICE         "notice"
-#define N_HAPTIC_EFFECT_MESSAGE        "message"
-#define N_HAPTIC_EFFECT_ATTENTION      "attention"
-#define N_HAPTIC_EFFECT_ALARM          "alarm"
-#define N_HAPTIC_EFFECT_RINGTONE       "ringtone"
+#define N_HAPTIC_EFFECT_DRAG_START      "drag_start"
+#define N_HAPTIC_EFFECT_RELEASE_WEAK    "release_weak"
+#define N_HAPTIC_EFFECT_DRAG_FAIL       "drag_fail"
+#define N_HAPTIC_EFFECT_DRAG_BOUNDARY   "drag_boundary"
+#define N_HAPTIC_EFFECT_TOUCH_WEAK      "touch_weak"
+#define N_HAPTIC_EFFECT_DRAG_END        "drag_end"
+#define N_HAPTIC_EFFECT_RELEASE         "release"
+#define N_HAPTIC_EFFECT_TOUCH           "touch"
+#define N_HAPTIC_EFFECT_RELEASE_STRONG  "release_strong"
+#define N_HAPTIC_EFFECT_TOUCH_STRONG    "touch_strong"
+#define N_HAPTIC_EFFECT_SHORT           "short"
+#define N_HAPTIC_EFFECT_STRONG          "strong"
+#define N_HAPTIC_EFFECT_LONG            "long"
+#define N_HAPTIC_EFFECT_NOTICE          "notice"
+#define N_HAPTIC_EFFECT_MESSAGE         "message"
+#define N_HAPTIC_EFFECT_ATTENTION       "attention"
+#define N_HAPTIC_EFFECT_ALARM           "alarm"
+#define N_HAPTIC_EFFECT_RINGTONE        "ringtone"
 
 /* Supported haptic classes */
-#define N_HAPTIC_CLASS_UNDEFINED  (0)
-#define N_HAPTIC_CLASS_TOUCH      (1)
-#define N_HAPTIC_CLASS_EVENT      (2)
+#define N_HAPTIC_CLASS_UNDEFINED        (0)
+#define N_HAPTIC_CLASS_TOUCH            (1)
+#define N_HAPTIC_CLASS_EVENT            (2)
 
 /**
  * Convenience function to filter haptic depending on settings and call state
